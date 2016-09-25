28af68871446e5689df26e5c3dd863cc8c3b8ef8
Ryan Efendy <ryanefendy95@gmail.com>
2016-09-25 07:33:06 -0400

    add spoiler.css

--- extension/js/embeded.js
+++ extension/js/embeded.js
@@ -1,27 +1,37 @@
 var content = $.find('._5yl5');
-var send = function (message, $elem) {
-    chrome.runtime.sendMessage({message: message}, {}, function(isHarass) {
-    	console.log(isHarass);
-    	if (isHarass) {
-    		$elem.text("I am a terrible human being.");
-    	}
-    });
+var send = function(message, $elem) {
+  chrome.runtime.sendMessage({
+    message: message
+  }, {}, function(isHarass) {
+    console.log(isHarass);
+    if (isHarass) {
+      var randomInt = Math.round(Math.random() * 100000);
+      var parent = $elem.parent();
+      $(parent).prepend('<div class="spoiler" id="' + randomInt + '">Potentially harsassing message. Click to show.</div>');
+      $("#" + randomInt).click(function() {
+        $("#" + randomInt).hide();
+        $elem.show();
+      });
+      $elem.hide();
+      var original = $elem.hide();
+    }
+  });
 };
 
-var handle = function (message) {
-	send($(message).text(), $(message));
-    console.log($(message).text());
+var handle = function(message) {
+  send($(message).text(), $(message));
+  console.log($(message).text());
 };
 
 var len = content.length;
 $('body').append('<div class="_5yl5"><span>test</span></div>');
-$(document).ready(function () {
-    // Faceboook.com
-    $("._5yl5 > span").initialize(function (indx, message) {
-        handle(message);
-    });
-    // Messenger.com
-    $("._3oh-").initialize(function (indx, message) {
-        handle(message);
-    });
+$(document).ready(function() {
+  // Faceboook.com
+  $("._5yl5 > span").initialize(function(indx, message) {
+    handle(message);
+  });
+  // Messenger.com
+  $("._3oh-").initialize(function(indx, message) {
+    handle(message);
+  });
 });
