aa56cb63693abd9754a20fcc2707c01ee4ccc37b
akw54 <akw54@drexel.edu>
2016-09-24 20:05:04 -0400

    bg script

---
+++ extension/js/background.js
@@ -0,0 +1,6 @@
+console.log("background");
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
+    sendResponse("recieved");
+});
--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,43 +1,4 @@
-var watson = require('watson-developer-cloud');
-
-var tone_analyzer = watson.tone_analyzer({
-  username: '74046f25-1dca-4b0f-9211-efb47dc49825',
-  password: 'fkiya1W5n0ce',
-  version: 'v3',
-  version_date: '2016-05-19 '
-});
-
-var test = function (content, len) {
-    $("._5yl5").initialize( function(){
-        console.log(content[len-1].innerText);
-    });
-
-    for (i = 0; i < len; i++) {
-        var message = content[i].innerText;
-        console.log(message);
-        tone_analyzer.tone({ text: message }, function(err, tone) {
-            $.post('http://harassblocker-yehyaawad.c9users.io:8080/check', function (res) {
-                console.log(res);
-            });
-        });
-    }
-}
-
 document.addEventListener('DOMContentLoaded', function() {
-    var content = $.find('._5yl5');
-    var len = content.length;
-    test(content, len);
-
-    for (i = 0; i < len; i++) {
-        var message = content[i].innerText;
-        console.log(message);
-        tone_analyzer.tone({ text: message }, function(err, tone) {
-            $.post('http://harassblocker-yehyaawad.c9users.io:8080/check', function (res) {
-                console.log(res);
-            });
-        });
-    }
-    
     $('#power').on('click', function () {
         if ($('#power').hasClass('glyphicon-eye-close')) {
             $('#power').removeClass('glyphicon-eye-close');
@@ -50,5 +11,5 @@ document.addEventListener('DOMContentLoaded', function() {
             $('#power').removeClass('text-muted');
             $('#power').addClass('text-danger');
         }
-    });
+    }); 
 });
