4ee1b212cf6e13f30fb10b53fd01a87a02387d5c
Yehya Awad <yha5009@psu.edu>
2016-09-24 19:27:56 -0500

    Fix 0 bug

--- extension/js/embeded.js
+++ extension/js/embeded.js
@@ -1,4 +1,4 @@
-alert("embeded");
+console.log("embeded");
 var content = $.find('._5yl5');
 var send = function (message) {
     chrome.runtime.sendMessage({message: message}, function(res) {
@@ -13,7 +13,7 @@ var len = content.length;
 // }
 $('body').append('<div class="_5yl5"><span>test</span></div>');
 $(document).ready(function () {
-    $("._5yl5 > span").initialize(function (message) {
+    $("._5yl5 > span").initialize(function (indx, message) {
         //send(message);
         console.log(message);
     });
